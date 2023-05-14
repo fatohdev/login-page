614d2a42e607c3d05675182a72da1675fce80932
fatoh-dev <fatohdev@gmail.com>
2023-05-14 22:35:26 +0300

    save initial password

--- login.js
+++ login.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const mysql=require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 
+require("dotenv").config();
 
-const app=express();
+const app = express();
 
-const db=mysql.createConnection({
-  host:'localhost',
-  user: 'root',
-  password:'Fatoh@360Nasr',
-  database:'acme'
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DATABASE_PASSWORD,
+  database: "acme",
 });
 db.connect();
-app.get('/users',(req,res) =>{
-  const sql= 'SELECT * FROM users WHERE age=25';
-  db.query(sql,(err, result)=>{
-    if(err) throw err;
+app.get("/users", (req, res) => {
+  const sql = "SELECT * FROM users WHERE age=25";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
     res.send(result);
-  })
+  });
 });
-
 
 app.listen(3000);
